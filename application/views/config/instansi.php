9143d9995d392c6a7b8b55f7821e4e97dec63f44
noplanalderson <noplanalderson@gmail.com>
2020-11-29 17:24:01 +0700

    Menampilkan versi Aplikasi

--- application/views/config/instansi.php
+++ application/views/config/instansi.php
@@ -41,7 +41,7 @@
                                 <div class="p-5">
                                     <img class="logo-sasuke" src="<?= site_url('assets/img/logo-default.png');?>">
                                     <div class="text-center mb-4">
-                                        <h1 class="h4 text-gray-900">SASUKE</h1>
+                                        <h1 class="h4 text-gray-900">SASUKE <?= SASUKE_VERSION ?></h1>
                                         <small>Konfigurasi Instansi</small>
                                     </div>
                                     <div id="msg_instansi" class="alert" style="display: none">
