6cf1c3c4cbf961f96d9257f34f3f6b5b1ad33151
noplanalderson <noplanalderson@gmail.com>
2020-12-04 23:34:03 +0700

    Perbaiki Card Title Tambah User

--- application/views/tambah-user.php
+++ application/views/tambah-user.php
@@ -15,7 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Daftar User</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Taambah User</h6>
                         </div>
                         <div class="card-body">
                             <?= form_open_multipart('', 'method="post" id="Userman" accept-charset="utf-8"');?>
