12b513eee7d8af6e4527d60703c3c1b407ea67ac
noplanalderson <noplanalderson@gmail.com>
2021-07-09 11:41:53 +0700

    Create empty database and email configuration

--- app/config/email.php
+++ app/config/email.php
@@ -2,11 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config = array(
-	'protocol' => "smtp",
-    'smtp_host' => "smtp.gmail.com", 
-    'smtp_port' => 465,
-    'smtp_user' => "noplanalderson@gmail.com",
-    'smtp_pass' => "Load1n9321!@#",
+	'protocol' => "",
+    'smtp_host' => "", 
+    'smtp_port' => 0,
+    'smtp_user' => "",
+    'smtp_pass' => "",
     'smtp_crypto' => 'ssl',
     'mailtype' => 'text/plan',
     'smtp_timeout' => '4',
