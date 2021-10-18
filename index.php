63169442257f2015044ed51bf31283e4793a6ff8
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2021-10-18 11:33:17 +0700

    Remove app version constant

--- index.php
+++ index.php
@@ -53,8 +53,6 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('SASUKE_VERSION', 'v3.0');
-	
 	// $_SERVER['CI_ENV'] = 'production';
 
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
