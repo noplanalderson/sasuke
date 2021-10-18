dfc57f54f7642743fdbaeaca22fa2cef8421961a
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2021-10-18 10:49:00 +0700

    Change version to 3.1.0 and change version format to semantic versioning

--- app/config/constants.php
+++ app/config/constants.php
@@ -84,4 +84,4 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-defined('SASUKE_VERSION')	   OR define('SASUKE_VERSION', 3.0);
+defined('SASUKE_VERSION')	   OR define('SASUKE_VERSION', '3.1.0');
